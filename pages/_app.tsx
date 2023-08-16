49cd35e466acc660d9c06b469442d97e95f77671
Yash Diwaker <190104076@hbtu.ac.in>
2023-08-16 18:23:58 +0530

    authentication flow done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from "next/font/google";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Toaster } from 'react-hot-toast';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const inter = Inter({ subsets: ["latin"] });
+const queryclient=new QueryClient()
 export default function App({ Component, pageProps }: AppProps) {
   return (
     < div className={inter.className} >
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryclient}>
+      <GoogleOAuthProvider clientId='611289805673-dbev6l710fsjpc3ejq1mh5n771829mvv.apps.googleusercontent.com'>
+        <Component {...pageProps} />
+          <Toaster />
+          <ReactQueryDevtools/>
+        </GoogleOAuthProvider>
+        </QueryClientProvider>
     </div>
   )
    
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,23 @@ import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
 import { BiHash, BiHomeCircle, BiMoney, BiUser } from "react-icons/bi";
 import { SlOptions } from "react-icons/sl";
 import FeedCard from "@/components/FeedCard";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { log } from "console";
+import { useCallback, useEffect, useState } from "react";
+import { toast } from "react-hot-toast/headless";
+import { graphqlClient } from "@/clients/api";
+import { verifyUserGoogleTokenQuery } from "@/graphql/query/user";
+import { useCurrentUser } from "@/hooks/user";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Home() {
+  const queryclient = useQueryClient();
+  const { user } = useCurrentUser();
+  const [profileUrl, setprofileUrl] = useState<any>("");
+  useEffect(() => {
+    setprofileUrl(user?.profileImageURL);
+  }, [user]);
+
   interface TwitterSidebarButton {
     title: string;
     icon: React.ReactNode;
@@ -45,10 +60,30 @@ export default function Home() {
     },
   ];
 
+  const handleLoginWithGoogle = useCallback(
+    async (cred: CredentialResponse) => {
+      const googleToken = cred.credential;
+      if (!googleToken) {
+        return toast.error("googleToken not found");
+      }
+      const { verifyGoogleToken } = await graphqlClient.request(
+        verifyUserGoogleTokenQuery,
+        { token: googleToken }
+      );
+
+      toast.success("verified success");
+      if (verifyGoogleToken) {
+        window.localStorage.setItem("__twitter_token", verifyGoogleToken);
+      }
+
+      await queryclient.invalidateQueries(["current-user"]);
+    },
+    [queryclient]
+  );
   return (
     <div>
       <div className="grid grid-cols-12 h-screen w-screen px-18">
-        <div className="col-span-3 p-4 ml-16 mt-[-6.2%]">
+        <div className="col-span-3 p-4 ml-16 mt-[-6.2%] relative">
           <div className="text-2xl px-4 py-3 w-fit cursor-pointer relative top-[1.1rem] left-[4.4%]  text-white transition-all ">
             <BsTwitter />
           </div>
@@ -69,6 +104,25 @@ export default function Home() {
               Tweet
             </button>
           </div>
+          {user && (
+            <div className="absolute mt-5 bottom-5 flex gap-2 text-white items-center bg-slate-800 py-2 px-4 rounded-full justify-between">
+              {user && (
+                <Image
+                  className="rounded-full"
+                  src={profileUrl}
+                  alt="user-Image"
+                  height={30}
+                  width={30}
+                />
+              )}
+              <div>
+                <h3 className="">
+                  {user.firstName}
+                  {user.lastName}
+                </h3>
+              </div>
+            </div>
+          )}
         </div>
         <div className="col-span-5 border-r-[1px] border-l-[1px] h-screen overflow-scroll no-scrollbar border-gray-600">
           <FeedCard />
@@ -80,7 +134,14 @@ export default function Home() {
           <FeedCard />
           <FeedCard />
         </div>
-        <div className="col-span-3"></div>
+        {!user && (
+          <div className="col-span-3 p-5">
+            <div className="p-5 bg-slate-700 rounded-lg">
+              <h2 className="my-2 "> New To Twitter?</h2>
+              <GoogleLogin onSuccess={handleLoginWithGoogle}></GoogleLogin>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
